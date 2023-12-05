d79e0d7a092b396f7b027f30f7e2289f960f5fa7
alexamen9 <ameneyroalex@gmail.com>
2023-12-04 18:22:24 -0600

    models

--- src/index.php
+++ src/index.php
@@ -4,7 +4,10 @@ require(__DIR__ . "/models/Db.php");
 try {
     $con = new Db();
     $db = $con->conection;
-    var_dump($db->query("SELECT 1+1"));
+    $a = $db->prepare("DESCRIBE task");
+    $a->execute();
+    $objeto = $stmt->fetch();
+    echo $objeto;
 } catch (PDOException $e) {
     print $e->getMessage();
 }
---
+++ src/models/User.php
@@ -0,0 +1,10 @@
+<?php
+class User { 
+    public $user_name;
+    public $user_email;
+
+    function createUser(){
+        
+    }
+
+}
