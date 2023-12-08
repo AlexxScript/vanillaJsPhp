9f40644834975c354cba6c2fd1768f0e884a7391
alexamen9 <ameneyroalex@gmail.com>
2023-12-07 21:05:17 -0600

    other commit

--- src/actions/createTasks.php
+++ src/actions/createTasks.php
@@ -4,8 +4,13 @@ require_once __DIR__ . "/../controllers/TaskController.php";
 require_once __DIR__ . "/../models/Task.php";
 
 session_start();
-$id = (int)$_SESSION["id_user"];
-print $id;
-$taskController = new TaskController();
-$tas = $taskController->createTask($id,"description2");
-var_dump($tas);
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    if (isset($_POST["send"])) {
+        $controllerTask = new TaskController();
+        $description = $_POST["description"];
+        $idUser = (int)$_SESSION["id_user"];
+        $controllerTask->createTask($idUser,$description);
+        header("Location:/tasks");
+    }
+}
--- src/components/formCreate.php
+++ src/components/formCreate.php
@@ -1,7 +1,7 @@
-<form action="../actions/createTasks.php">
+<form action="../actions/createTasks.php" method="post">
     <div class="inputClass">
         <label for="">Write a description</label>
-        <textarea name="description" placeholder="Write your task" class="area"></textarea>
+        <textarea name="description" placeholder="Write your task" class="area" required></textarea>
         <input type="submit" value="Create Task" name="send">
     </div>
 </form>
