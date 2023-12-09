8e9798593d294c986e4ff55774fe9037c8e466a1
AlexScript <ameneyroalex@gmail.com>
2023-12-08 23:56:27 -0600

    colors add

--- src/components/formCreate.php
+++ src/components/formCreate.php
@@ -1,6 +1,6 @@
 <form class="formTask" action="../actions/createTasks.php" method="post">
+<label for="">Write a description</label>
     <div class="inputClass">
-        <label for="">Write a description</label>
         <textarea name="description" placeholder="Write your task" class="area" required></textarea>
         <input type="submit" value="Create Task" name="send">
     </div>
