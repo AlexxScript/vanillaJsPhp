48554881af5d08e041fe97a4f1e2b6db25b483fb
alexamen9 <ameneyroalex@gmail.com>
2023-12-10 23:12:08 -0600

    some changes

--- src/views/home.php
+++ src/views/home.php
@@ -1,12 +1,20 @@
 <?php
 session_start();
 if (!isset($_SESSION["id_user"])) {
-    header("Location:/login");
-}
 ?>
+    <?php require_once __DIR__ . "/../components/nav.php" ?>
 
-<?php require_once __DIR__ . "/../components/nav.php" ?>
-<div class="mainContainer">
-    <h1><?php echo $_SESSION["user_name"] ?></h1>
-</div>
-<?php require_once __DIR__ . "/../components/footer.php" ?>
+    <div class="mainContainerHome homeContainer">
+        <h1>You must log in</h1>
+    </div>
+    <?php require_once __DIR__ . "/../components/footer.php" ?>
+
+<?php } else { ?>
+
+    <?php require_once __DIR__ . "/../components/nav.php" ?>
+    <div class="mainContainerHome homeContainer">
+        <h1><?php echo $_SESSION["user_name"] ?></h1>
+    </div>
+    <?php require_once __DIR__ . "/../components/footer.php" ?>
+
+<?php } ?>
