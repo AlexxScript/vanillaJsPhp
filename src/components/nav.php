82f8863ea764a74a61a9c7b385152dad265560e4
alexamen9 <ameneyroalex@gmail.com>
2023-12-12 23:55:47 -0600

    responsive design

--- src/components/nav.php
+++ src/components/nav.php
@@ -13,7 +13,12 @@
     <?php if (isset($_SESSION["id_user"])) { ?>
 
         <nav>
-            <ul>
+            <div class="buttonNav">
+                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5" />
+                </svg>
+            </div>
+            <ul id="nav">
                 <li><a href="/tasks">Tasks</a></li>
                 <li><a href="/">Home</a></li>
                 <li><a href="/logout">Log out</a></li>
@@ -24,7 +29,12 @@
 
     <?php if (!isset($_SESSION["id_user"])) { ?>
         <nav>
-            <ul>
+            <div class="buttonNav">
+                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5" />
+                </svg>
+            </div>
+            <ul id="nav">
                 <li><a href="/login">Login</a></li>
                 <li><a href="/register">Register</a></li>
                 <li><a href="/">Home</a></li>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
-const tex = document.querySelector(".mainContainerHome");
+const button = document.querySelector(".buttonNav");
+const nav = document.getElementById("nav")
 
-document.addEventListener("mousemove",function (e) {
-    this.querySelectorAll(".mainContainerHome").forEach((element) => {
-        console.log(element)
-      });
+button.addEventListener("click", function(e) {
+  nav.classList.toggle("ulActive")
+});
+
+document.addEventListener("mousemove", function (e) {
+  this.querySelectorAll(".mainContainerHome").forEach((element) => {
+    const x = (window.innerWidth - e.pageX *5) / 90;
+    const y = (window.innerHeight - e.pageY *5) / 90;
+
+    element.style.transform = `translateX(${x}px) translateY(${y}px)`;
+  });
 })
 
